29b606f4eb62c2e4f15f6079223b2b0e92784908
Goboitumelo Mpuru <goboitumelo.mpuru@standardbank.co.za>
2024-05-08 08:45:49 +0200

    view amd modifiers app

---
+++ HackingWithSwift/SwiftUI/project3/ViewsAndModifiers/ViewsAndModifiers/ViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiersApp.swift
+//  ViewsAndModifiers
+//
+//  Created by Goboitumelo on 6/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
