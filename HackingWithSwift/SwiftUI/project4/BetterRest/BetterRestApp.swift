7132d010eb2c83b2df43caf4f208424b5fc33f57
Goboitumelo Mpuru <goboitumelo.mpuru@standardbank.co.za>
2024-05-08 08:48:39 +0200

    betterRest App

---
+++ HackingWithSwift/SwiftUI/project4/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Goboitumelo on 15/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
