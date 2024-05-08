92b89178e299da88c55141c245720d1cfcdd3095
Goboitumelo Mpuru <goboitumelo.mpuru@standardbank.co.za>
2024-05-08 08:40:52 +0200

    project 2 guesst the flag app

---
+++ HackingWithSwift/SwiftUI/project2/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Goboitumelo on 02/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
