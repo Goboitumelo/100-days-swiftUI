dc528bd51f28352def7e39e4b6283bad73cc395a
Goboitumelo Mpuru <goboitumelo.mpuru@standardbank.co.za>
2024-06-06 09:02:48 +0200

    project7

---
+++ HackingWithSwift/SwiftUI/project7/Project7/Petition.swift
@@ -0,0 +1,15 @@
+//
+//  Petition.swift
+//  Project7
+//
+//  Created by Goboitumelo Mpuru on 3/06/2024.
+//
+//
+
+import Foundation
+
+struct Petition: Codable {
+    var title: String
+    var body: String
+    var signatureCount: Int
+}
---
+++ HackingWithSwift/SwiftUI/project7/Project7/Petitions.swift
@@ -0,0 +1,13 @@
+//
+//  Petitions.swift
+//  Project7
+//
+//  Created by Goboitumelo Mpuru on 3/06/2024.
+//
+//
+
+import Foundation
+
+struct Petitions: Codable {
+    var results: [Petition]
+}
