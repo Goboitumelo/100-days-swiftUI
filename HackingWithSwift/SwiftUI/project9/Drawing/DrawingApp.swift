b8ccae3f319bb2ea7ccc3bc2ce98a931900b0b19
Goboitumelo Mpuru <goboitumelo.mpuru@standardbank.co.za>
2024-06-07 10:11:07 +0200

    drawing app project 9

---
+++ HackingWithSwift/SwiftUI/project9/Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Goboitumelo Mpuru on 06/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
