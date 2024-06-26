0afd1127e3c63baf824b80625fffb473bc50d757
Goboitumelo Mpuru <goboitumelo.mpuru@standardbank.co.za>
2024-06-26 08:28:13 +0200

    project 13 installer app

---
+++ HackingWithSwift/SwiftUI/project13/Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Goboitumelo Mpuru on 18/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
