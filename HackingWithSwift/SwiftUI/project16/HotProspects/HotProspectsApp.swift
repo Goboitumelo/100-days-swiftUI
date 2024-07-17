fe46386ff9f4187b67ed21770dc6a77e6a56d9e0
Goboitumelo Mpuru <goboitumelo.mpuru@standardbank.co.za>
2024-07-17 10:17:37 +0200

    project 16

---
+++ HackingWithSwift/SwiftUI/project16/HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Goboitumelo Mpuru on 14/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
