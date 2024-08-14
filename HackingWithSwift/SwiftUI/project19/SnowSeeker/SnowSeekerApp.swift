879a14162b753eea8512c54f01c4da1c755eab07
Goboitumelo Mpuru <goboitumelo.mpuru@standardbank.co.za>
2024-08-14 09:45:47 +0200

    project19

---
+++ HackingWithSwift/SwiftUI/project19/SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by Goboitumle Mpuru on 05/08/20224.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
